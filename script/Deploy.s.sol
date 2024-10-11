8df896b9fce993fc32cbd625dc1b8dd30902b21d
Shreyansh <Shreyann56sh@gmail.com>
2024-10-11 19:52:34 +0530

    contract changes

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,14 +3,38 @@ pragma solidity ^0.8.19;
 
 import "./DeployHelpers.s.sol";
 import {DeployYourContract} from "./DeployYourContract.s.sol";
+import "../contracts/BLSToken.sol"; // Adjust path based on your file structure
+import "../contracts/BlumeLiquidStaking.sol"; // Adjust path based on your file structure;
+
 
 contract DeployScript is ScaffoldETHDeploy {
     function run() external {
-        DeployYourContract deployYourContract = new DeployYourContract();
-        deployYourContract.run();
+        uint256 deployerPrivateKey = vm.envUint("ADMIN_PRIVATE_KEY");
+        uint256 owner = vm.envUint("OWNER_ADDRESS");
+        address admin = vm.addr(deployerPrivateKey);
+        // Start the broadcast (send transactions)
+        if (deployerPrivateKey == 0) {
+            revert InvalidPrivateKey(
+                "You don't have a deployer account. Make sure you have set DEPLOYER_PRIVATE_KEY in .env or use `yarn generate` to generate a new random account"
+            );
+        }
+        if (owner == 0) {
+            revert InvalidOwnerAddress();
+        }
+        vm.startBroadcast(deployerPrivateKey);
+        address setter = vm.addr(deployerPrivateKey);
 
-        // deploy more contracts here
-        // DeployMyContract deployMyContract = new DeployMyContract();
-        // deployMyContract.run();
+        // Deploy BLSToken
+        BLSToken blsToken = new BLSToken();
+
+        // Deploy BlumeLiquidStaking
+        BlumeLiquidStaking stakingToken = new BlumeLiquidStaking(blsToken);
+
+        // Stop broadcasting
+        vm.stopBroadcast();
+
+        // Print the addresses of the deployed contracts
+        console.log("BLSToken deployed to:", address(blsToken));
+        console.log("BlumeLiquidStaking deployed to:", address(stakingToken));;
     }
 }
